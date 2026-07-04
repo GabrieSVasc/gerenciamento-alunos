f35b459b7e605da6c7db641c4a0e799ffec3b4e8
GabrieSVasc <vascgabrie@gmail.com>
2026-07-04 03:20:34 -0300

    Testes de Integração
    Implementando os testes de integração de usuário e aluno

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,91 @@
 package br.com.gerenciamento.controller;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+
+@SuppressWarnings("unchecked")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoControllerTest {
+	@Autowired
+	private AlunoController alunoController;
+	
+	@Test
+	public void pesquisarAlunoRetornaListagemCompletaTest() throws Exception {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Jorge");
+		aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        BindingResult bindingResult =
+                new BeanPropertyBindingResult(aluno, "aluno");
+        this.alunoController.inserirAluno(aluno, bindingResult);
+		ModelAndView mv = this.alunoController.pesquisarAluno("");
+		List<Aluno> listaAlunos = (List<Aluno>) mv.getModel().get("ListaDeAlunos");
+		Assert.assertFalse(listaAlunos.isEmpty());
+	}
+	
+	@Test
+	public void pesquisarApenasUmAlunoTest() throws Exception {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Maria");
+		aluno.setTurno(Turno.NOTURNO);
+		aluno.setCurso(Curso.INFORMATICA);
+		aluno.setStatus(Status.ATIVO);
+		aluno.setMatricula("987654");
+		BindingResult b = new BeanPropertyBindingResult(aluno, "aluno");
+		this.alunoController.inserirAluno(aluno, b);
+		ModelAndView mv = this.alunoController.pesquisarAluno("Maria");
+		List<Aluno> alunosRetorno = (List<Aluno>) mv.getModel().get("ListaDeAlunos");
+		Assert.assertEquals(1, alunosRetorno.size());
+	}
+	
+	@Test
+	public void editandoAlunoComSucessoTest() throws Exception {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Eduardo");
+		aluno.setTurno(Turno.NOTURNO);
+		aluno.setCurso(Curso.CONTABILIDADE);
+		aluno.setStatus(Status.INATIVO);
+		aluno.setMatricula("2020");
+		BindingResult b = new BeanPropertyBindingResult(aluno, "aluno");
+		this.alunoController.inserirAluno(aluno, b);
+		aluno.setStatus(Status.ATIVO);
+		this.alunoController.editar(aluno);
+		ModelAndView mv = this.alunoController.pesquisarAluno("Eduardo");
+		List<Aluno> alunos = (List<Aluno>) mv.getModel().get("ListaDeAlunos");
+		Assert.assertEquals(Status.ATIVO, alunos.get(0).getStatus());
+	}
+	
+	@Test
+	public void editandoAlunoInexistenteTest() throws Exception {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Miriam");
+		aluno.setTurno(Turno.NOTURNO);
+		aluno.setCurso(Curso.CONTABILIDADE);
+		aluno.setStatus(Status.ATIVO);
+		aluno.setMatricula("40256");
+		BindingResult b = new BeanPropertyBindingResult(aluno, "aluno");
+		this.alunoController.inserirAluno(aluno, b);
+		this.alunoController.removerAluno(aluno.getId());
+		ModelAndView mv = this.alunoController.pesquisarAluno("Miriam");
+		List<Aluno> alunos = (List<Aluno>) mv.getModel().get("ListaDeAlunos");
+		Assert.assertTrue(alunos.isEmpty());
+
+	}
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,73 @@
 package br.com.gerenciamento.controller;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import br.com.gerenciamento.util.Util;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+	
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@Autowired
+	private ServiceUsuario serviceUsuario;
+	
+	@Autowired
+	private UsuarioController usuarioController;
+	
+	@Test
+	public void realizarLoginInformacoesInvalidasTest() throws Exception {
+		mockMvc.perform(post("/login")
+				.param("user", "Username")
+				.param("senha", "SenhaErrada"))
+				.andExpect(view().name("login/cadastro"));
+	}
+	
+	@Test
+	public void realizarLoginComSucessoTest() throws Exception, Exception {
+		Usuario user = new Usuario();
+		user.setEmail("email@gmail.com");
+		user.setSenha("Senha");
+		user.setUser("Username");
+        
+        this.serviceUsuario.salvarUsuario(user);
+
+        mockMvc.perform(post("/login")
+                        .param("user", "Username")
+                        .param("senha", "Senha"))
+                		.andExpect(view().name("home/index"));
+	}
+	
+	@Test
+	public void cadastraUmUsuarioComSucesso() throws Exception {
+		mockMvc.perform(post("/salvarUsuario")
+				.param("user", "Username1")
+				.param("senha", "Senha1")
+				.param("email", "Mail@gmail.com"))
+				.andExpect(view().name("redirect:/"));
+	}
+
+	@Test
+	public void realizaLogoutComSucesso() throws Exception {
+		mockMvc.perform(post("/logout"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("login/login"));
+	}
+
 }
